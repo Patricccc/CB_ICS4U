a2d9c61277217aa5129c51e705fafa3fc93c90cf
Patrick Yang <patricc.yang@gmail.com>
2022-11-01 19:14:09 -0400

    Update 107d.cpp

--- Unit 1: Pointers and Arrays/107d.cpp
+++ Unit 1: Pointers and Arrays/107d.cpp
@@ -1,18 +1,17 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-void init_array(int a[], int i){
-    if(i == 0) return;
+void init_array(int* a, int i){
     a[i] = rand() % 50 + 1;
+    if(i == 0) return;
     init_array(a, i - 1);
 }
 
-int main() {
-    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-    srand(0);
-    int a[10];
+int main(){
+    srand(time(NULL));
+    int* a = new int[10];
     init_array(a, 9);
-    for(int i = 0; i < 10; i++) cout << a[i];
+    for(int i = 0; i < 10; i++) cout << a[i] << endl;
     return 0;
 }
